bd9f0bfcc1e6cd6f4c548cc4e8550c29f261835c
Johnathan Speck <johnathans96@gmail.com>
2018-07-17 20:16:01 -0400

    new firmware and serialmonitor

--- SerialMonitor.cs
+++ SerialMonitor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
 using System.Windows.Threading;
+using System.Text;
 
 namespace NintendoSpy
 {
@@ -10,9 +11,12 @@ namespace NintendoSpy
 
     public class SerialMonitor
     {
-        StreamWriter writetext = new StreamWriter(File.Open("debug.txt", FileMode.Create));
+        //StreamWriter debug = new StreamWriter(File.Open("debug.txt", FileMode.Create));
         const int BAUD_RATE = 115200;
-        const int TIMER_MS  = 1;       // originally 30
+        const int TIMER_MS  = 5;       // originally 30
+
+        byte[] packet = new byte[4];
+        byte[] translatedPacket = new byte[32];
 
         public event PacketEventHandler PacketReceived;
         public event EventHandler Disconnected;
@@ -21,6 +25,14 @@ namespace NintendoSpy
         List <byte> _localBuffer;
 
         DispatcherTimer _timer;
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2} ", b);
+            return hex.ToString();
+        }
 
         public SerialMonitor (string portName)
         {
@@ -64,34 +76,64 @@ namespace NintendoSpy
             // If there's an IOException then the device has been disconnected.
             try {
                 int readCount = _datPort.BytesToRead;
-                writetext.Flush();
-                writetext.WriteLine(readCount);
-                if (readCount < 1) return;
-                byte[] readBuffer = new byte [readCount];
-                _datPort.Read (readBuffer, 0, readCount);
+                if (readCount < 4) return;
+                byte[] readBuffer = new byte [4];
+                _datPort.Read (readBuffer, 0, 4);
                 _datPort.DiscardInBuffer ();
                 _localBuffer.AddRange (readBuffer);
+                //debug.WriteLine(((int)readBuffer[0]).ToString() + "\t" + ((int)readBuffer[1]).ToString() + "\t" + ((int)readBuffer[2]).ToString() + "\t" + ((int)readBuffer[3]).ToString());
             }
             catch (IOException) {
                 Stop ();
-                writetext.Close();
+                //debug.Close();
                 if (Disconnected != null) Disconnected (this, EventArgs.Empty);
                 return;
             }
 
-            // Try and find 2 splitting characters in our buffer.
-            int lastSplitIndex = _localBuffer.LastIndexOf (0x0A);
-            if (lastSplitIndex <= 1) return;
-            int sndLastSplitIndex = _localBuffer.LastIndexOf (0x0A, lastSplitIndex - 1);
-            if (lastSplitIndex == -1) return;
+            packet = _localBuffer.GetRange(0, 4).ToArray();
+            //debug.WriteLine(((int)packet[0]).ToString() + "\t" + ((int)packet[1]).ToString() + "\t" + ((int)packet[2]).ToString() + "\t" + ((int)packet[3]).ToString());
 
-            // Grab the latest packet out of the buffer and fire it off to the receive event listeners.
-            int packetStart = sndLastSplitIndex + 1;
-            int packetSize  = lastSplitIndex - packetStart;
-            PacketReceived (this, _localBuffer.GetRange (packetStart, packetSize).ToArray ());
+            // 2 3 0 1
+            translatedPacket[0] = (byte)((packet[2] & 128) >> 7);
+            translatedPacket[1] = (byte)((packet[2] & 64)  >> 6);
+            translatedPacket[2] = (byte)((packet[2] & 32)  >> 5);
+            translatedPacket[3] = (byte)((packet[2] & 16)  >> 4);
+            translatedPacket[4] = (byte)((packet[2] & 8)   >> 3);
+            translatedPacket[5] = (byte)((packet[2] & 4)   >> 2);
+            translatedPacket[6] = (byte)((packet[2] & 2)   >> 1);
+            translatedPacket[7] = (byte)((packet[2] & 1)   >> 0);
+
+            translatedPacket[8] = 0;
+            translatedPacket[9] = 0;
+            translatedPacket[10] = (byte)((packet[3] & 32) >> 5);
+            translatedPacket[11] = (byte)((packet[3] & 16) >> 4);
+            translatedPacket[12] = (byte)((packet[3] & 8)  >> 3);
+            translatedPacket[13] = (byte)((packet[3] & 4)  >> 2);
+            translatedPacket[14] = (byte)((packet[3] & 2)  >> 1);
+            translatedPacket[15] = (byte)((packet[3] & 1)  >> 0);
+
+            translatedPacket[16] = (byte)((packet[0] & 128) >> 7);
+            translatedPacket[17] = (byte)((packet[0] & 64)  >> 6);
+            translatedPacket[18] = (byte)((packet[0] & 32)  >> 5);
+            translatedPacket[19] = (byte)((packet[0] & 16)  >> 4);
+            translatedPacket[20] = (byte)((packet[0] & 8)   >> 3);
+            translatedPacket[21] = (byte)((packet[0] & 4)   >> 2);
+            translatedPacket[22] = (byte)((packet[0] & 2)   >> 1);
+            translatedPacket[23] = (byte)((packet[0] & 1)   >> 0);
+
+            translatedPacket[24] = (byte)((packet[1] & 128) >> 7);
+            translatedPacket[25] = (byte)((packet[1] & 64)  >> 6);
+            translatedPacket[26] = (byte)((packet[1] & 32)  >> 5);
+            translatedPacket[27] = (byte)((packet[1] & 16)  >> 4);
+            translatedPacket[28] = (byte)((packet[1] & 8)   >> 3);
+            translatedPacket[29] = (byte)((packet[1] & 4)   >> 2);
+            translatedPacket[30] = (byte)((packet[1] & 2)   >> 1);
+            translatedPacket[31] = (byte)((packet[1] & 1)   >> 0);
+
+            PacketReceived(this, translatedPacket);
 
             // Clear our buffer up until the last split character.
-            _localBuffer.RemoveRange (0, lastSplitIndex);
+            _localBuffer.RemoveRange(0, 4);
         }
     }
 }
---
+++ firmware/n64.ino
@@ -0,0 +1,137 @@
+#define SERIAL_BUFFER_SIZE 256
+
+#define PIN_READ( pin )  (PIND&(1<<(pin)))
+#define PINC_READ( pin ) (PINC&(1<<(pin)))
+#define MICROSECOND_NOPS "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"
+
+#define WAIT_FALLING_EDGE( pin ) while( !PIN_READ(pin) ); while( PIN_READ(pin) );
+
+#define MODEPIN_N64  1
+
+#define N64_PIN        2
+#define N64_PREFIX     9
+#define N64_BITCOUNT  32
+
+#define ZERO  '\0'  // Use a byte value of 0x00 to represent a bit with value 0.
+#define ONE    '1'  // Use an ASCII one to represent a bit with value 1.  This makes Arduino debugging easier.
+#define SPLIT '\n'  // Use a new-line character to split up the controller state packets.
+
+
+
+
+// Declare some space to store the bits we read from a controller.
+unsigned char rawData[ 256 ];
+
+// 8 bytes of data that we get from the controller
+struct state{
+    char stick_x;
+    char stick_y;
+    // bits: 0, 0, 0, start, y, x, b, a
+    unsigned char data1;
+    // bits: 1, L, R, Z, Dup, Ddown, Dright, Dleft
+    unsigned char data2;
+} N64_status;
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// General initialization, just sets all pins to input and starts serial communication.
+void setup()
+{
+    PORTD = 0x00;
+    DDRD  = 0x00;
+    PORTC = 0xFF; // Set the pull-ups on the port we use to check operation mode.
+    DDRC  = 0x00;
+    Serial.begin( 115200 );
+}
+
+void translate_raw_data()
+{
+    // The get_N64_status function sloppily dumps its data 1 bit per byte
+    // into the get_status_extended char array. It's our job to go through
+    // that and put each piece neatly into the struct N64_status
+    memset(&N64_status, 0, sizeof(N64_status));
+    // line 1
+    // bits: A, B, Z, Start, Dup, Ddown, Dleft, Dright
+    // line 2
+    // bits: 0, 0, L, R, Cup, Cdown, Cleft, Cright
+    // line 3
+    // bits: joystick x value
+    // These are 8 bit values centered at 0x80 (128)
+    // line 4
+    // bits: joystick 4 value
+    // These are 8 bit values centered at 0x80 (128)
+    for (int i=0; i<8; i++) {
+        N64_status.data1 |= rawData[i+9] ? (0x80 >> i) : 0;
+        N64_status.data2 |= rawData[8+i+9] ? (0x80 >> i) : 0;
+        N64_status.stick_x |= rawData[16+i+9] ? (0x80 >> i) : 0;
+        N64_status.stick_y |= rawData[24+i+9] ? (0x80 >> i) : 0;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Performs a read cycle from one of Nintendo's one-wire interface based controllers.
+// This includes the N64 and the Gamecube.
+//     pin  = Pin index on Port D where the data wire is attached.
+//     bits = Number of bits to read from the line.
+template< unsigned char pin >
+void read_oneWire( unsigned char bits )
+{
+    unsigned char *rawDataPtr = rawData;
+
+read_loop:
+
+    // Wait for the line to go high then low.
+    WAIT_FALLING_EDGE( pin );
+
+    // Wait ~2us between line reads
+    asm volatile( MICROSECOND_NOPS MICROSECOND_NOPS );
+
+    // Read a bit from the line and store as a byte in "rawData"
+    *rawDataPtr = PIN_READ(pin);
+    ++rawDataPtr;
+    if( --bits == 0 ) return;
+
+    goto read_loop;
+}
+
+// Verifies that the 9 bits prefixing N64 controller data in 'rawData'
+// are actually indicative of a controller state signal.
+inline bool checkPrefixN64 ()
+{
+    if( rawData[0] != 0 ) return false; // 0
+    if( rawData[1] != 0 ) return false; // 0
+    if( rawData[2] != 0 ) return false; // 0
+    if( rawData[3] != 0 ) return false; // 0
+    if( rawData[4] != 0 ) return false; // 0
+    if( rawData[5] != 0 ) return false; // 0
+    if( rawData[6] != 0 ) return false; // 0
+    if( rawData[7] == 0 ) return false; // 1
+    if( rawData[8] == 0 ) return false; // 1
+    return true;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Sends a packet of controller data over the Arduino serial interface.
+inline void sendRawData( unsigned char first, unsigned char count )
+{
+    for( unsigned char i = first ; i < first + count ; i++ ) {
+        Serial.write( rawData[i] ? ONE : ZERO );
+    }
+    Serial.write( SPLIT );
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Arduino sketch main loop definition.
+void loop()
+{
+    noInterrupts();
+    read_oneWire< N64_PIN >( N64_PREFIX + N64_BITCOUNT );
+    interrupts();
+    if( checkPrefixN64() ) {
+        translate_raw_data();
+        Serial.write((uint8_t *)&N64_status,4);
+        delay(10);
+    }
+}
