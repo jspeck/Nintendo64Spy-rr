0b355d981ffa36b7a8c6983f9661b1df1cb97356
Johnathan Speck <johnathans96@gmail.com>
2020-08-25 22:47:19 -0500

    Update n64_uno.ino

--- firmware/n64_uno.ino
+++ firmware/n64_uno.ino
@@ -2,27 +2,22 @@
 
 #define SERIAL_BUFFER_SIZE 256
 
+// nintendospy
 #define PIN_READ( pin )  (PIND&(1<<(pin)))
-#define PINC_READ( pin ) (PINC&(1<<(pin)))
 #define MICROSECOND_NOPS "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"
-
 #define WAIT_FALLING_EDGE( pin ) while( !PIN_READ(pin) ); while( PIN_READ(pin) );
-
-#define MODEPIN_N64  1
-
 #define N64_PIN        2
 #define N64_PREFIX     9
 #define N64_BITCOUNT  32
-
-#define ZERO  '\0'  // Use a byte value of 0x00 to represent a bit with value 0.
-#define ONE    '1'  // Use an ASCII one to represent a bit with value 1.  This makes Arduino debugging easier.
-#define SPLIT '\n'  // Use a new-line character to split up the controller state packets.
-
-
-
-
-// Declare some space to store the bits we read from a controller.
 unsigned char rawData[ 256 ];
+
+// tasbot
+//#define N64_QUERY (PIND & 0x01)
+#define N64_QUERY (PIND&(1<<(N64_PIN)))
+static char n64_raw_dump[38]; // maximum recv is 1+2+32 bytes + 1 bit
+static unsigned char n64_command;
+static unsigned char n64_buffer[33];
+static void get_n64_command();
 
 // 8 bytes of data that we get from the controller
 struct state{
@@ -34,15 +29,12 @@ struct state{
     unsigned char data2;
 } N64_status;
 
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // General initialization, just sets all pins to input and starts serial communication.
 void setup()
 {
     PORTD = 0x00;
     DDRD  = 0x00;
-    PORTC = 0xFF; // Set the pull-ups on the port we use to check operation mode.
-    DDRC  = 0x00;
     Serial.begin( 115200 );
 }
 
@@ -63,10 +55,10 @@ void translate_raw_data()
     // bits: joystick 4 value
     // These are 8 bit values centered at 0x80 (128)
     for (int i=0; i<8; i++) {
-        N64_status.data1 |= rawData[i+9] ? (0x80 >> i) : 0;
-        N64_status.data2 |= rawData[8+i+9] ? (0x80 >> i) : 0;
-        N64_status.stick_x |= rawData[16+i+9] ? (0x80 >> i) : 0;
-        N64_status.stick_y |= rawData[24+i+9] ? (0x80 >> i) : 0;
+        N64_status.data1 |= rawData[i] ? (0x80 >> i) : 0;
+        N64_status.data2 |= rawData[8+i] ? (0x80 >> i) : 0;
+        N64_status.stick_x |= rawData[16+i] ? (0x80 >> i) : 0;
+        N64_status.stick_y |= rawData[24+i] ? (0x80 >> i) : 0;
     }
 }
 
@@ -96,44 +88,141 @@ read_loop:
     goto read_loop;
 }
 
-// Verifies that the 9 bits prefixing N64 controller data in 'rawData'
-// are actually indicative of a controller state signal.
-inline bool checkPrefixN64 ()
-{
-    if( rawData[0] != 0 ) return false; // 0
-    if( rawData[1] != 0 ) return false; // 0
-    if( rawData[2] != 0 ) return false; // 0
-    if( rawData[3] != 0 ) return false; // 0
-    if( rawData[4] != 0 ) return false; // 0
-    if( rawData[5] != 0 ) return false; // 0
-    if( rawData[6] != 0 ) return false; // 0
-    if( rawData[7] == 0 ) return false; // 1
-    if( rawData[8] == 0 ) return false; // 1
-    return true;
-}
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Sends a packet of controller data over the Arduino serial interface.
-inline void sendRawData( unsigned char first, unsigned char count )
-{
-    for( unsigned char i = first ; i < first + count ; i++ ) {
-        Serial.write( rawData[i] ? ONE : ZERO );
-    }
-    Serial.write( SPLIT );
-}
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Arduino sketch main loop definition.
-void loop()
-{
-    noInterrupts();
-    read_oneWire< N64_PIN >( N64_PREFIX + N64_BITCOUNT );
-    interrupts();
-    if( checkPrefixN64() ) {
-        translate_raw_data();
-        Serial.write((uint8_t *)&N64_status,4);
-        delay(10);
+void loop(){
+    // wait to make sure the line is idle before
+    // we begin listening
+        
+    for (int idle_wait=32; idle_wait>0; --idle_wait) {
+        if (!N64_QUERY) {
+            idle_wait = 32;
+        }
     }
+
+    noInterrupts();
+    // Wait for incomming 64 command
+    // this will block until the N64 sends us a command
+    get_n64_command();
+
+    // 0x00 is identify command
+    // 0x01 is status
+    // 0x02 is read
+    // 0x03 is write
+    switch (n64_command)
+    {
+        case 0x00:
+        case 0x01:
+            read_oneWire< N64_PIN >(N64_BITCOUNT);
+            interrupts();
+            translate_raw_data();
+            Serial.write((uint8_t *)&N64_status,4);
+            delay(10);
+            break;
+        default:
+            interrupts();
+            break;
+    }
+}
+
+/**
+  * Waits for an incomming signal on the N64 pin and reads the command,
+  * and if necessary, any trailing bytes.
+  * 0x00 is an identify request
+  * 0x01 is a status request
+  * 0x02 is a controller pack read
+  * 0x03 is a controller pack write
+  *
+  * for 0x02 and 0x03, additional data is passed in after the command byte,
+  * which is also read by this function.
+  *
+  * All data is raw dumped to the n64_raw_dump array, 1 bit per byte, except
+  * for the command byte, which is placed all packed into n64_command
+  */
+static void get_n64_command()
+{
+    int bitcount;
+    char *bitbin = n64_raw_dump;
+
+    n64_command = 0;
+
+    bitcount = 8;
+
+read_loop:
+        // wait for the line to go low
+        while (N64_QUERY){}
+
+        // wait approx 2us and poll the line
+        asm volatile (
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                );
+        if (N64_QUERY)
+            n64_command |= 0x01;
+
+        --bitcount;
+        if (bitcount == 0)
+            goto read_more;
+
+        n64_command <<= 1;
+
+        // wait for line to go high again
+        // I don't want this to execute if the loop is exiting, so
+        // I couldn't use a traditional for-loop
+        while (!N64_QUERY) {}
+        goto read_loop;
+
+read_more:
+        switch (n64_command)
+        {
+            case (0x03):
+                // write command
+                // we expect a 2 byte address and 32 bytes of data
+                bitcount = 272 + 1; // 34 bytes * 8 bits per byte
+                //Serial.println("command is 0x03, write");
+                break;
+            case (0x02):
+                // read command 0x02
+                // we expect a 2 byte address
+                bitcount = 16 + 1;
+                //Serial.println("command is 0x02, read");
+                break;
+            case (0x00):
+            case (0x01):
+            default:
+                // get the last (stop) bit
+                bitcount = 1;
+                break;
+        }
+
+        // make sure the line is high. Hopefully we didn't already
+        // miss the high-to-low transition
+        while (!N64_QUERY) {}
+read_loop2:
+        // wait for the line to go low
+        while (N64_QUERY){}
+
+        // wait approx 2us and poll the line
+        asm volatile (
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                );
+        *bitbin = N64_QUERY;
+        ++bitbin;
+        --bitcount;
+        if (bitcount == 0)
+            return;
+
+        // wait for line to go high again
+        while (!N64_QUERY) {}
+        goto read_loop2;
 }
